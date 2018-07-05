01bc6f1c9ef24ed3a99c2a255b447565523e13fc
marshal20 <x.madkour@gmail.com>
2018-07-05 20:44:19 +0200

    added DgramPacketsBroadcaster example

---
+++ Examples/DgramPacketsBroadcaster/main.cpp
@@ -0,0 +1,26 @@
+#include <Network/Address.hpp>
+#include <Network/Socket.hpp>
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	const char* port = "2000";
+	if (argc == 2)
+		port = argv[2];
+
+	Address localhostTarget = Address::broadcast;
+	localhostTarget.setPort(std::stoi(port));
+
+	Socket sock(Socket::Type::Dgram);
+	sock.beBroadcast();
+
+	char buff[1025];
+	while (std::cin.getline(buff, sizeof(buff)))
+	{
+		sock.sendto(buff, strlen(buff) + 1, localhostTarget);
+		std::cout << "- sent " << strlen(buff) + 1 << " Bytes, to: " << localhostTarget << ", buff: " << buff << std::endl;
+	}
+
+	return 0;
+}
