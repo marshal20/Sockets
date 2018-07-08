0ec5e809e29d6364a7e12fef2b9fdb510621da5e
marshal20 <x.madkour@gmail.com>
2018-07-08 16:09:52 +0200

    fixe AddressOperations example and added additional Addresses including IPv6

--- Examples/AddressOperations/main.cpp
+++ Examples/AddressOperations/main.cpp
@@ -8,9 +8,13 @@ int main(int argc, char* argv[])
 	std::cout << "IPv6 test: " << Address().setIP(IPv6({ 1,2,3,4,5,6,7,8 })) << std::endl;
 	std::cout << "IPv4 test str: " << Address().fromPresentation("1.2.3.4") << std::endl;
 	std::cout << "IPv6 test str: " << Address().fromPresentation("1:2:3:4:5:6:7:8") << std::endl;
-	std::cout << "localhost Address: " << Address::localhost << std::endl;
-	std::cout << "Broadcast Address: " << Address::broadcast << std::endl;
-	std::cout << "Hostname test: " << Address::fromPresentation("google.com") << std::endl;
+	std::cout << "localhost Address: " << Address::localhost() << std::endl;
+	std::cout << "thishost Address: " << Address::thishost() << std::endl;
+	std::cout << "Broadcast Address: " << Address::broadcast() << std::endl;
+	std::cout << "localhost Address IPv6: " << Address::localhost(Protocol::IPv6) << std::endl;
+	std::cout << "thishost Address IPv4: " << Address::thishost(Protocol::IPv6) << std::endl;
+	std::cout << "google.com: " << Address::fromPresentation("google.com") << std::endl;
+	std::cout << "example.com: " << Address::fromPresentation("example.com") << std::endl;
 
 	return 0;
 }
