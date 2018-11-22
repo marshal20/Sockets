83a7e1ffe5c980122b4522d6fd3c25a1ab8aad3b
marshal20 <x.madkour@gmail.com>
2018-11-22 20:10:24 +0200

    Fixed AddressOperations example

--- Examples/AddressOperations/main.cpp
+++ Examples/AddressOperations/main.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, char* argv[]) try
 {
-	std::cout << "IPv4 test: " << Address().setIP(IPv4({ 1,2,3,4 })) << std::endl;
-	std::cout << "IPv6 test: " << Address().setIP(IPv6({ 1,2,3,4,5,6,7,8 })) << std::endl;
-	std::cout << "IPv4 test str: " << Address().fromPresentation("1.2.3.4") << std::endl;
-	std::cout << "IPv6 test str: " << Address().fromPresentation("1:2:3:4:5:6:7:8") << std::endl;
+	std::cout << "IPv4 test: " << Address(IPv4({ 1,2,3,4 })) << std::endl;
+	std::cout << "IPv6 test: " << Address(IPv6({ 1,2,3,4,5,6,7,8 })) << std::endl;
+	std::cout << "IPv4 test str: " << Address::fromPresentation("1.2.3.4") << std::endl;
+	std::cout << "IPv6 test str: " << Address::fromPresentation("1:2:3:4:5:6:7:8") << std::endl;
 	std::cout << "localhost Address: " << Address::localhost() << std::endl;
 	std::cout << "thishost Address: " << Address::thishost() << std::endl;
 	std::cout << "Broadcast Address: " << Address::broadcast() << std::endl;
